eabd2f2da5cef85396ce7f0dd93d996566396925
Hitachi006 <183986215+Hitachi006@users.noreply.github.com>
2024-12-11 09:27:16 +0000

    happiness commit

--- autolysis.py
+++ autolysis.py
@@ -15,7 +15,7 @@ file_name="".join(sys.argv[1:]).split('.')[0]
 
 # 2 create a new directory with filename
 new_dir = os.path.join(os.getcwd(), file_name)
-#print(new_dir)
+print(new_dir)
 os.makedirs(new_dir, exist_ok=True)
   
 # 3 Perform an automated analysis and create a README file
@@ -24,7 +24,7 @@ os.makedirs(new_dir, exist_ok=True)
   
 # 4 create a README.md file
 #with open(os.path.join(new_dir,'README.md'), 'w') as f:
- f.write(df_anal.to_markdown())
+#f.write(df_anal.to_markdown())
 
 #print(f)
 
