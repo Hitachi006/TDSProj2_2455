4fde30f9b39480528622251fbf5c9b33bfc36d70
Hitachi006 <183986215+Hitachi006@users.noreply.github.com>
2024-12-11 05:30:20 +0000

    initial version

--- autolysis.py
+++ autolysis.py
@@ -6,28 +6,28 @@
 
 #import pandas as pd
 #import numpy as np
-#import os
+import os
 import sys
 
 
-def create_dir(file_path):
-  # 1 extract filename
-  file_name=os.path.splitext(os.path.basename(file_path)[0])
+# 1 extract filename
+file_name="".join(sys.argv[1:]).split('.')[0]
 
-  # 2 create a new directory with filename
-  new_dir = os.path.join(os.getcwd(), file_name)
-  os.makedirs(new_dir, exist_ok=True)
+# 2 create a new directory with filename
+new_dir = os.path.join(os.getcwd(), file_name)
+#print(new_dir)
+os.makedirs(new_dir, exist_ok=True)
   
-  # 3 Perform an automated analysis and create a README file
-  df=pd.read_csv(file_path)
-  df_anal=df.describe()
+# 3 Perform an automated analysis and create a README file
+#df=pd.read_csv(file_path)
+#df_anal=df.describe()
   
-  # 4 create a README.md file
+# 4 create a README.md file
      
-  with open(os.path.join(new_dir,'README.md'), 'w') as f:
-    f.write(df_anal.to_markdown())
+#with open(os.path.join(new_dir,'README.md'), 'w') as f:
+#f.write(df_anal.to_markdown())
 
-  return(f)
+#return(f)
 
   
   
