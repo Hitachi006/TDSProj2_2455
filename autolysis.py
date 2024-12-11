48777ba212849c6a47f960c7fccad66a50212e10
Hitachi006 <183986215+Hitachi006@users.noreply.github.com>
2024-12-11 10:34:56 +0000

    media commit

--- autolysis.py
+++ autolysis.py
@@ -11,20 +11,23 @@ import sys
 
 
 # 1 extract filename
-file_name="".join(sys.argv[1:]).split('.')[0]
+file_path="".join(sys.argv[1:])
+file_name=os.path.splitext(os.path.basename(file_path))[0]
 
 # 2 create a new directory with filename
 new_dir = os.path.join(os.getcwd(), file_name)
 print(new_dir)
+print(file_path)
 os.makedirs(new_dir, exist_ok=True)
   
 # 3 Perform an automated analysis and create a README file
-#df=pd.read_csv('happiness.csv')
-#df_anal=df.describe()
+df1=pd.read_csv(file_path,encoding='latin-1')
+df1_anal=df1.describe()
+
   
 # 4 create a README.md file
-#with open(os.path.join(new_dir,'README.md'), 'w') as f:
-#f.write(df_anal.to_markdown())
+with open(os.path.join(new_dir,'README.md'), 'w') as f:
+    f.write(df1_anal.to_markdown())
 
 #print(f)
 
